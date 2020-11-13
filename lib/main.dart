df015482b98f7533df73723f2b58c6703d23ef0d
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-13 12:20:25 +0100

    Refactoring code for receive two new components

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,72 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
 void main() => runApp(QuizApp());
 
-class QuizAppState extends State<QuizApp> {
-  int perguntaSelecionada = 0;
+class _QuizAppState extends State<QuizApp> {
+  int _perguntaSelecionada = 0;
+  final List<Map<String, Object>> _perguntas = const [
+    {
+      "question": "What's your favorite color?",
+      "answer": ['Black', 'Red', 'Green', 'Blue'],
+    },
+    {
+      "question": "What's your favorite animal?",
+      "answer": ['Bird', 'Dog', 'Cat', 'Snake'],
+    },
+    {
+      "question": "What's your favorite country?",
+      "answer": ['Germany', 'Brazil', 'Spain', 'EUA'],
+    }
+  ];
 
-  void pergunta() {
-    setState(() => {
-      perguntaSelecionada++
-    });
-    print(perguntaSelecionada);
+  void _pergunta() {
+    if (hasSelectedQuestion) {
+      setState(() => {_perguntaSelecionada++});
+    }
   }
-  
+
+  bool get hasSelectedQuestion {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final List<String> perguntas = [
-      "What's your favorite color?",
-      "What's your favorite animal?",
-    ];
+    List<String> answers =
+        hasSelectedQuestion ? _perguntas[_perguntaSelecionada]['answer'] : null;
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Perguntas'),
+          title: Text('Questions'),
           centerTitle: true,
         ),
-        body: Column(
-          children: [
-            Text(perguntas[perguntaSelecionada]),
-            RaisedButton(
-              child: Text('Resposta 1'),
-              onPressed: pergunta,
-            ),
-            RaisedButton(
-              child: Text('Resposta 2'),
-              onPressed: pergunta,
-            ),
-            RaisedButton(
-              child: Text('Resposta 3'),
-              onPressed: pergunta,
-            ),
-          ],
-        ),
+        body: hasSelectedQuestion
+            ? Column(
+                children: [
+                  Question(_perguntas[_perguntaSelecionada]['question']),
+                  ...answers
+                      .map((answer) => Answer(answer, _pergunta))
+                      .toList(),
+                ],
+              )
+            : Center(
+                child: Text(
+                  'Congrats!',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
       ),
     );
   }
 }
+
 class QuizApp extends StatefulWidget {
   @override
-  QuizAppState createState() {
-    return QuizAppState();
+  _QuizAppState createState() {
+    return _QuizAppState();
   }
-
 }
